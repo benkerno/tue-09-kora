498b7ab01269392948144b1c207e20695fd9442c
benkerno <59324854+benkerno@users.noreply.github.com>
2024-07-16 10:58:31 +1000

    Create part1.c

---
+++ part1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+struct my_struct {
+    int number;
+    char letter;
+    double another_number;
+}
+
+int main(void) {
+    // int num;
+    // int input;
+    // scanf("%d", &input);
+    // arr[input-1] = 'c';
+    
+    struct my_struct *strct = malloc(sizeof(struct my_struct));
+    free(arr)
+}
+
